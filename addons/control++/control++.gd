28286c40f92ba101dbf473de27efc23699643157
MUTEDGALAXY <53514993+MUTEDGALAXY@users.noreply.github.com>
2021-10-29 09:37:15 +1100

    Edited main plugin script
    Edited main plugin script to add nodes into godot more "naturally"

--- addons/control++/control++.gd
+++ addons/control++/control++.gd
@@ -1,10 +1,11 @@
 tool
 extends EditorPlugin
 
-
 func _enter_tree() -> void:
-	pass
-
+	add_custom_type("CircleContainer", "Container", preload("res://addons/control++/circle_container.gd"), preload("res://addons/control++/CircleContainer.svg"))
+	add_custom_type("FixedGrid", "GridContainer", preload("res://addons/control++/fixed_grid.gd"), preload("res://addons/control++/FixedGrid.svg"))
+	add_custom_type("HBoxContainer++", "HBoxContainer", preload("res://addons/control++/h_box_container++.gd"), null)
+	add_custom_type("VBoxContainer++", "VBoxContainer", preload("res://addons/control++/v_box_container++.gd"), null)
 
 func _exit_tree() -> void:
 	pass
